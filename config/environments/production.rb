3b6d8000cb57553dc27017b80338f44352ea93ee
succesom <omelchenko_ig@mail.ru>
2014-06-10 23:26:28 +0300

    assets path added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Domreg::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile += %w( search.js )
   config.assets.precompile = [/^[a-zA-Z]*\..*/]
+  config.assets.paths << Rails.root.join("public/assets")
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
